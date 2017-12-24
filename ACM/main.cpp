d9628b9110529ca7666156b859025fb90eac6fdf
ObserverFrost <2322162863@qq.com>
2017-12-24 20:27:58 +0800

    new

---
+++ ACM/main.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <string>
+# include <vector>
+using namespace std;
+
+int main(int argc,char *argv[]) {
+    int *p = nullptr;
+    cout << !p << endl;
+    string str = "000009999";
+    cout << stol(str);
+    return 0;
+}
